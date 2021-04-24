63810a981c45e5259c1b7c362b6a5e2d02da0432
ashrafuzzaman93 <skshahid.im@gmail.com>
2021-04-24 21:08:57 +0600

    Fixed responsive issues & added humberger menu

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
+
 const Logo = () => {
+	let [humberger, handleHubberger] = useState(false);
+
 	return (
 		<div className="brand-logo">
 			<a href="/">
 				<img src="./images/logo.png" width="134" height="31" alt="DiFood" />
 			</a>
+
+			<button
+				className={`humberger ${humberger ? "nav-open" : ""}`}
+				onClick={() => handleHubberger((humberger = !humberger))}
+			>
+				<span></span>
+				<span></span>
+				<span></span>
+			</button>
 		</div>
 	);
 };
